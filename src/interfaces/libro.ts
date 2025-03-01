a7e63cef350960de3e916c66af0817eb91b7965d
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2025-03-01 18:25:40 +0100

    Auto-generated commit

--- src/interfaces/libro.ts
+++
@@ -1,7 +0,0 @@
-export interface Libro {
-  titulo : string;
-  num_paginas : number;
-  anno_publicacion : number;
-}
-
-
--- src/pipe/puntitos.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PuntitosPipe } from './puntitos.pipe';
-
-describe('PuntitosPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PuntitosPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
